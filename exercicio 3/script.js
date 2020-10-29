1db5e2bcaf95ef2274f5e0601f56ddbbf276df66
Jhoel Gonzalez <jhoel834@gmail.com>
2020-10-29 00:00:27 -0430

    adicionando arquivo script.js do exercio 3

---
+++ exercicio 3/script.js
@@ -0,0 +1,37 @@
+function Person(nome, sobreNome, idade) {
+    this.firstName = nome
+    this.lastName = sobreNome
+    this.age = idade
+}
+
+function oldest(array) {
+    let maximunAge = 0
+    let oldestPerson = 0
+
+    for (let person of array) {
+        if (person.age > maximunAge) {
+            maximunAge = person.age
+            oldestPerson = `${person.firstName} ${person.lastName}`
+        }
+    } return oldestPerson
+}
+
+let personArray = []
+let repetition = 0
+do {
+    let nome = prompt("Qual é o seu nome?")
+    let sobreNome = prompt("Qual é o seu sobrenome?")
+    let idade = prompt("Qual é a sua idade")
+
+    personArray[repetition] = new Person(nome, sobreNome, idade)
+
+    repetition = repetition + 1
+
+} while (confirm("Voce deseja adicionar mais uma pessoa?"))
+
+console.log('Registros de:')
+for (let personRegistration of personArray) {
+    console.log(`${personRegistration.firstName} ${personRegistration.lastName}: ${personRegistration.age}`)
+}
+    
+console.log(`A pessoa mais velha é: ${oldest(personArray)}`)
