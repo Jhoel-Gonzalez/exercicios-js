7c0d6a5d8d98d9c68bd5b06fb0872cf25b1030d7
Jhoel Gonzalez <jhoel834@gmail.com>
2020-10-31 09:08:51 -0430

    Quebrando o script em funcoes para maior organizacao

--- exercicio 5/script.js
+++ exercicio 5/script.js
@@ -1,20 +1,33 @@
-let form = document.querySelector('#form')
-let resultado = 0
-form.addEventListener("submit", (event) => {
-    event.preventDefault()
-    let cep = form.elements.cep.value
-    
-    fetch("https://viacep.com.br/ws/" + cep + "/json/").then(resposta => {
-        if (resposta.ok) {
-            return resposta
-        } else {
-            throw new Error(`Codigo ${resposta.status}`)
-        }
-    }).then(resposta => resposta.json()).then(data => resultado = data).catch(error => console.log(error))
+const form = document.querySelector('#form')
 
-    let endereco = "O seu endereco é: " + resultado.logradouro + ", " + resultado.bairro + ", " + resultado.localidade + ", " + resultado.uf
-    let paragrafo = document.createElement("p")
+function mostraResultado(resultado){
+    const endereco = `O seu endereco é: ${resultado.logradouro}, ${resultado.bairro}, ${resultado.localidade}, ${resultado.uf}`
+    const paragrafo = document.createElement("p")
     paragrafo.textContent = endereco
 
     document.body.appendChild(paragrafo)
+}
+
+function chamaApi(cep) {
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    .then(resposta => {
+        if (resposta.ok) {
+            return resposta.json()
+        } else {
+            throw new Error(`Codigo ${resposta.status}`)
+        }
+    })
+    .then(data => {
+        mostraResultado(data)
+       
+    })
+    .catch(error => {
+        console.log(error)
+    })
+}
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const cep = form.elements.cep.value
+    chamaApi(cep)
 })
